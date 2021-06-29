99ce9e88ad41b77acd788a49bb09670faa4a7832
Seinlin Li <seinlin.likaiostech.com>
2021-06-29 01:10:44 -0700

    Bug 124332 - Update vendored source for appscmd. r=fabrice.desre

--- update-vendor.sh
+++ update-vendor.sh
@@ -12,7 +12,3 @@ cargo vendor -- third-party > .cargo/config
 
 # Unused for now.
 rm -rf third-party/breakpad_sys/breakpad/
-
-# Remove useless windows libraries
-rm -rf third-party/winapi-x86_64-pc-windows-gnu/lib
-rm -rf third-party/winapi-i686-pc-windows-gnu/lib
