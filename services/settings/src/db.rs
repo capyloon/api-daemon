1ffd3fffafd3b509e179e57f4292ce31c59b2955
Seinlin Li <seinlin.li@kaiostech.com>
2021-07-12 17:43:38 -0700

    Bug 124713 - Update setting db to support default value. r=fabrice.desre

--- services/settings/src/db.rs
+++ services/settings/src/db.rs
@@ -3,7 +3,7 @@ use crate::generated::common::*;
 use common::observers::ObserverTracker;
 use common::traits::DispatcherId;
 use common::JsonValue;
-use log::{error, info};
+use log::{debug, error, info};
 use rusqlite::{named_params, params, Connection, NO_PARAMS};
 use serde_json::Value;
 use sqlite_utils::{DatabaseUpgrader, SqliteDb};
@@ -15,6 +15,13 @@ const DB_PATH: &str = "./settings.sqlite";
 const DB_PATH: &str = "/data/local/service/api-daemon/settings.sqlite";
 
 const TABLE_NAME: &str = "settings";
+
+static UPGRADE_0_1_SQL: [&str; 1] = [r#"CREATE TABLE IF NOT EXISTS settings (
+                                        name TEXT UNIQUE,
+                                        value TEXT)"#];
+
+static UPGRADE_1_2_SQL: [&str; 1] = [r#"ALTER TABLE settings
+                                        ADD COLUMN default_value TEXT"#];
 
 #[derive(Error, Debug)]
 pub enum Error {
@@ -38,20 +45,34 @@ pub struct SettingsSchemaManager {}
 
 impl DatabaseUpgrader for SettingsSchemaManager {
     fn upgrade(&mut self, from: u32, to: u32, connection: &mut Connection) -> bool {
-        // We only support version 1 currently.
-        if !(from == 0 && to == 1) {
+        // We only support version 2 currently.
+        if to != 2 {
             return false;
         }
 
-        connection
-            .execute(
-                &format!(
-                    "CREATE TABLE IF NOT EXISTS {} ( name TEXT UNIQUE, value TEXT)",
-                    TABLE_NAME
-                ),
-                NO_PARAMS,
-            )
-            .is_ok()
+        let mut current = from;
+
+        macro_rules! execute_commands {
+            ($from:expr, $cmds:expr) => {
+                if current == $from && current < to {
+                    for cmd in $cmds {
+                        if let Err(err) = connection.execute(cmd, NO_PARAMS) {
+                            error!("Upgrade step failure: {}", err);
+                            return false;
+                        }
+                    }
+                    current += 1;
+                }
+            };
+        }
+        // Upgrade from version 0.
+        execute_commands!(0, &UPGRADE_0_1_SQL);
+
+        // Upgrade from version 1.
+        execute_commands!(1, &UPGRADE_1_2_SQL);
+
+        // At the end, the current version should match the expected one.
+        current == to
     }
 }
 
@@ -79,7 +100,7 @@ pub struct SettingsDb {
 impl SettingsDb {
     pub fn new(event_broadcaster: SettingsFactoryEventBroadcaster) -> Self {
         // TODO: manage error opening the db.
-        let db = SqliteDb::open(DB_PATH, &mut SettingsSchemaManager {}, 1).unwrap();
+        let db = SqliteDb::open(DB_PATH, &mut SettingsSchemaManager {}, 2).unwrap();
         if let Err(err) = db.enable_wal() {
             error!("Failed to enable WAL mode on settings db: {}", err);
         }
@@ -140,12 +161,20 @@ impl SettingsDb {
     }
 
     pub fn set(&mut self, settings: &[SettingInfo]) -> Result<(), Error> {
+        self.set_internal(settings, false)
+    }
+
+    fn set_internal(&mut self, settings: &[SettingInfo], is_default: bool) -> Result<(), Error> {
         let tx = self.db.mut_connection().transaction()?;
         {
             let mut stmt_del =
                 tx.prepare(&format!("DELETE FROM {} WHERE name=:name", TABLE_NAME))?;
             let mut stmt_ins = tx.prepare(&format!(
                 "INSERT OR REPLACE INTO {}(name, value) VALUES(:name, :value)",
+                TABLE_NAME
+            ))?;
+            let mut stmt_default = tx.prepare(&format!(
+                "INSERT OR REPLACE INTO {}(name, value, default_value) VALUES(:name, :value, :default_value)",
                 TABLE_NAME
             ))?;
 
@@ -160,9 +189,19 @@ impl SettingsDb {
                     // Store the string representation
                     // TODO: check if we should use SQlite json support.
                     let serialized = serde_json::to_string(&value).unwrap_or_else(|_| "{}".into());
-                    stmt_ins.execute_named(
-                        named_params! {":name": setting_info.name, ":value": serialized},
-                    )?;
+                    if is_default {
+                        debug!(
+                            "Set {} default value: {}",
+                            setting_info.name, &serialized
+                        );
+                        stmt_default.execute_named(
+                            named_params! {":name": setting_info.name, ":value": serialized, ":default_value": serialized},
+                        )?;
+                    } else {
+                        stmt_ins.execute_named(
+                            named_params! {":name": setting_info.name, ":value": serialized},
+                        )?;
+                    }
                 }
 
                 // Dispatch a change event for this setting.
@@ -170,20 +209,55 @@ impl SettingsDb {
                     .broadcast_change(setting_info.clone());
 
                 // If we have observers for this setting, call their callback.
-                self.observers.for_each(&setting_info.name, |obs, _id| {
-                    match obs {
+                self.observers
+                    .for_each(&setting_info.name, |obs, _id| match obs {
                         ObserverType::Proxy(cb) => {
                             cb.callback(setting_info.clone());
                         }
                         ObserverType::FuncPtr(cb) => {
                             cb.callback(&setting_info.name, &setting_info.value);
                         }
-                    }
-                });
+                    });
             }
         }
         tx.commit()?;
         Ok(())
+    }
+
+    // Check if a setting needs to be migrated to the new default value.
+    // In:
+    //      name: setting name
+    //      new_default: the new default value in settings.json
+    // Return:
+    //      true/false
+    pub fn need_migrate(&self, name: &str, new_default: &Value) -> bool {
+        if new_default == &Value::Null {
+            return false;
+        }
+
+        let mut stmt = match self
+            .db
+            .connection()
+            .prepare(&format!("SELECT * FROM {} WHERE name=:name", TABLE_NAME))
+        {
+            Ok(stmt) => stmt,
+            Err(_) => return false,
+        };
+
+        let new_serialized = serde_json::to_string(new_default).unwrap_or_else(|_| "{}".into());
+        let (current, default) = stmt
+            .query_row_named(named_params! {":name": name}, |r| {
+                let value: String = r.get("value").unwrap_or_default();
+                let default_value: String = r.get("default_value").unwrap_or_default();
+                Ok((value, default_value))
+            })
+            .unwrap(); // It is safe to unwrap() as it always return Ok.
+
+        if new_serialized == default {
+            return false;
+        }
+
+        current == default
     }
 
     pub fn get(&self, name: &str) -> Result<JsonValue, Error> {
@@ -230,18 +304,24 @@ impl SettingsDb {
             // setttings that are new.
             let settings: Vec<SettingInfo> = map
                 .iter()
-                .filter_map(|(name, value)| {
-                    if self.get(name).is_err() {
-                        Some(SettingInfo {
-                            name: name.clone(),
-                            value: JsonValue::from((*value).clone()),
-                        })
-                    } else {
-                        None
+                .filter_map(|(name, value)| match self.get(name) {
+                    Err(_) => Some(SettingInfo {
+                        name: name.clone(),
+                        value: JsonValue::from((*value).clone()),
+                    }),
+                    Ok(_) => {
+                        if self.need_migrate(name, value) {
+                            Some(SettingInfo {
+                                name: name.clone(),
+                                value: JsonValue::from((*value).clone()),
+                            })
+                        } else {
+                            None
+                        }
                     }
                 })
                 .collect();
-            self.set(&settings)?;
+            self.set_internal(&settings, true)?;
             Ok(settings.len())
         } else {
             Err(Error::InvalidImport)
@@ -289,14 +369,24 @@ fn import_settings() {
     );
     assert_eq!(*db.get("alarm.enabled").unwrap(), Value::Bool(false));
 
+    let _ = db.set(&[SettingInfo {
+        name: "powersave.enabled".to_string(),
+        value: JsonValue::from(Value::Bool(true)),
+    }]);
+    assert_eq!(*db.get("powersave.enabled").unwrap(), Value::Bool(true));
+
     // Import a new settings file with only one new setting.
     let settings =
         serde_json::from_reader(std::fs::File::open("./test-fixtures/settings_2.json").unwrap())
             .unwrap();
-    assert_eq!(db.merge_json(&settings).unwrap(), 1);
+    assert_eq!(db.merge_json(&settings).unwrap(), 3);
     assert_eq!(
         *db.get("a.new.setting").unwrap(),
         Value::String("Hello World!".into())
+    );
+    assert_eq!(
+        *db.get("audio.volume.alarm").unwrap(),
+        Value::Number(10.into())
     );
 
     let mut values = db
@@ -304,15 +394,18 @@ fn import_settings() {
             String::from("app.update.battery-threshold.unplugged"),
             String::from("phone.dtmf.type"),
             String::from("alarm.enabled"),
+            String::from("powersave.enabled"),
         ])
         .unwrap();
     values.sort_by(|a, b| (a.name).cmp(&b.name));
 
-    assert_eq!(values.len(), 3);
+    assert_eq!(values.len(), 4);
     assert_eq!(values[0].name, "alarm.enabled");
     assert_eq!(values[1].name, "app.update.battery-threshold.unplugged");
     assert_eq!(values[2].name, "phone.dtmf.type");
+    assert_eq!(values[3].name, "powersave.enabled");
     assert_eq!(*values[0].value, Value::Bool(false));
-    assert_eq!(*values[1].value, Value::Number(25.into()));
+    assert_eq!(*values[1].value, Value::Number(30.into()));
     assert_eq!(*values[2].value, Value::String("long".into()));
+    assert_eq!(*values[3].value, Value::Bool(true));
 }
