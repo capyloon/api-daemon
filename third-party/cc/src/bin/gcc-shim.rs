1c1d504327135f39fea5d9f89604f73e073133ed
Fabrice Desré <fabrice@desre.org>
2023-06-09 15:18:10 -0700

    Downgrade cc in order to un-break MacOS cross compilation.

--- third-party/cc/src/bin/gcc-shim.rs
+++ third-party/cc/src/bin/gcc-shim.rs
@@ -2,7 +2,7 @@
 
 use std::env;
 use std::fs::File;
-use std::io::{self, prelude::*};
+use std::io::prelude::*;
 use std::path::PathBuf;
 
 fn main() {
@@ -10,61 +10,39 @@ fn main() {
     let program = args.next().expect("Unexpected empty args");
 
     let out_dir = PathBuf::from(
-        env::var_os("GCCTEST_OUT_DIR")
-            .unwrap_or_else(|| panic!("{}: GCCTEST_OUT_DIR not found", program)),
+        env::var_os("GCCTEST_OUT_DIR").expect(&format!("{}: GCCTEST_OUT_DIR not found", program)),
     );
 
     // Find the first nonexistent candidate file to which the program's args can be written.
-    let candidate = (0..).find_map(|i| {
-        let candidate = out_dir.join(format!("out{}", i));
+    for i in 0.. {
+        let candidate = &out_dir.join(format!("out{}", i));
 
+        // If the file exists, commands have already run. Try again.
         if candidate.exists() {
-            // If the file exists, commands have already run. Try again.
-            None
-        } else {
-            Some(candidate)
+            continue;
         }
-    }).unwrap_or_else(|| panic!("Cannot find the first nonexistent candidate file to which the program's args can be written under out_dir '{}'", out_dir.display()));
 
-    // Create a file and record the args passed to the command.
-    let f = File::create(&candidate).unwrap_or_else(|e| {
-        panic!(
-            "{}: can't create candidate: {}, error: {}",
+        // Create a file and record the args passed to the command.
+        let mut f = File::create(candidate).expect(&format!(
+            "{}: can't create candidate: {}",
             program,
-            candidate.display(),
-            e
-        )
-    });
-    let mut f = io::BufWriter::new(f);
-
-    (|| {
+            candidate.to_string_lossy()
+        ));
         for arg in args {
-            writeln!(f, "{}", arg)?;
+            writeln!(f, "{}", arg).expect(&format!(
+                "{}: can't write to candidate: {}",
+                program,
+                candidate.to_string_lossy()
+            ));
         }
-
-        f.flush()?;
-
-        let mut f = f.into_inner()?;
-        f.flush()?;
-        f.sync_all()
-    })()
-    .unwrap_or_else(|e| {
-        panic!(
-            "{}: can't write to candidate: {}, error: {}",
-            program,
-            candidate.display(),
-            e
-        )
-    });
+        break;
+    }
 
     // Create a file used by some tests.
     let path = &out_dir.join("libfoo.a");
-    File::create(path).unwrap_or_else(|e| {
-        panic!(
-            "{}: can't create libfoo.a: {}, error: {}",
-            program,
-            path.display(),
-            e
-        )
-    });
+    File::create(path).expect(&format!(
+        "{}: can't create libfoo.a: {}",
+        program,
+        path.to_string_lossy()
+    ));
 }
